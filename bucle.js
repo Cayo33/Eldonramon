91cf04acf0a94caef484657d1b9d069db2ac819b
Cayo33 <oro38507@gmail.com>
2022-03-07 14:16:06 -0600

    first commit

---
+++ bucle.js
@@ -0,0 +1,11 @@
+for(let i = 0;i+1 < 10;i++){
+   const number = i+1;
+   console.log(`Tabla del ${number}`);
+
+  for(let i = 0;i < 10;i++){
+      const m = i+1;
+      const result = number * m;
+      //2 X 1 = 2
+      console.log(`${number} X ${m} = ${result}`);
+    }
+}
