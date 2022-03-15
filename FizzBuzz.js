b6d5bd0ebcacd52e3f4aabab777e0779539278f4
Cayo33 <oro38507@gmail.com>
2022-03-14 18:49:18 -0600

    Tarea #1

---
+++ FizzBuzz.js
@@ -0,0 +1,13 @@
+for( let i = 0;i <100; i++) {
+    
+     const number = i+1;
+    if(number % 3 === 0 && number % 5 === 0  ) {
+         console.log( `FizzBuzz` );
+    } else if(number % 3 === 0) {
+        console.log( `Fizz`);
+    } else if(number % 5 === 0) {
+            console.log(`Buzz`);
+    } else if(number);
+           console.log(`${number}`);
+
+}
